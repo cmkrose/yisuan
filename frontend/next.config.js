e9a64d182565231f32286055ba5e28489e6a9fe1
yisuan <admin@yisuan.com>
2026-07-15 00:04:56 +0800

    跳过类型检查

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
 
+  typescript: { ignoreBuildErrors: true },
+
   poweredByHeader: false,
   compress: true,
   generateEtags: true,
